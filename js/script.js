b17b8d5d41495a93ed75db9cf8bec79afa78e7ba
Denis <heratub@inbox.ru>
2023-04-30 22:49:04 +0300

    js012 homework

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '123');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const answers = () => {
+    let answer1 = prompt("Один из последних просмотренных фильмов?",'');
+    let answer2 = prompt("На сколько оцените его?",'');
+    personalMovieDB.movies[answer1] = answer2;
+};
+
+answers();
+answers();
+
+console.log(personalMovieDB);
