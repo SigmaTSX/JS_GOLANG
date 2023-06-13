6aebf56014498d852c8d25e99b20a05034f2de75
Denis <heratub@inbox.ru>
2023-06-13 14:23:20 +0300

    jsx практика

--- react_low/my-app/src/index.js
+++ react_low/my-app/src/index.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app';
 import './index.css';
-//import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const Header  = () => <h2>Hello World!</h2>
-const styleField = {
-  width: '300px'
-}
-const Field  = () => {
-  const holder = "Enter here"
-  return <input 
-          style={styleField}
-          type="text" 
-          placeholder={holder}
-          autoComplete=""
-          className='first' 
-        />
-}
-const Btn = () => {
-  const text = "Log in";
-  const logged = true;
-  return <button>{logged ? "Enter" : text}</button>
-}
+
 root.render(
   <React.StrictMode>
-    <Header />
-    <Field />
-    <Btn />
+    <App/>
   </React.StrictMode>
 );
 
