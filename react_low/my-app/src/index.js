c9dfd9a72ddb28ca1713760827a5eba93eb199a9
Denis <heratub@inbox.ru>
2023-06-13 13:14:52 +0300

    jsx теория

--- react_low/my-app/src/index.js
+++ react_low/my-app/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const Header  = () => <h2>Hello World!</h2>
+const styleField = {
+  width: '300px'
+}
+const Field  = () => {
+  const holder = "Enter here"
+  return <input 
+          style={styleField}
+          type="text" 
+          placeholder={holder}
+          autoComplete=""
+          className='first' 
+        />
+}
+const Btn = () => {
+  const text = "Log in";
+  const logged = true;
+  return <button>{logged ? "Enter" : text}</button>
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Field />
+    <Btn />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
